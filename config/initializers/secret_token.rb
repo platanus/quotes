b757f7203c089f03158941c25c8e48050fc0145c
Andres Arellano <andres.arellano@gmail.com>
2013-09-11 10:50:26 -0400

    Fist commit: test env created

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Quotes::Application.config.secret_key_base = '7ce4fefc3b13b497aad6ee4500a1fadd121a7d0e070da944ebe40694dc67fc67652b834ca3a158436baf917c4ee259bd7282658c2ff368fa09ae67417e3a236b'
